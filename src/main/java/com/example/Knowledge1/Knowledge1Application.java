a23dc391df189bb04ed9be4d3f01a1a440529992
Ram2603 <rambondre9793@gmail.com>
2025-01-11 22:54:32 +0530

    newcode

---
+++ src/main/java/com/example/Knowledge1/Knowledge1Application.java
@@ -0,0 +1,13 @@
+package com.example.Knowledge1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Knowledge1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Knowledge1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Knowledge1/Knowledge1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Knowledge1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Knowledge1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
