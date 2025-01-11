d0ae63583ca47d28a913fc3265bc1e270dc4b157
Ram2603 <rambondre9793@gmail.com>
2025-01-11 23:32:21 +0530

    Update train3.java

--- src/main/java/com/example/Knowledge1/train3.java
+++ src/main/java/com/example/Knowledge1/train3.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class train3 {
     @GetMapping("/train3")
-    public String getData() {return  "Please book your hotel ticket for New Delhi 50% discount" ; }
+    public String getData() {return  "Please book your hotel ticket for New Delhi 80% discount" ; }
 }
